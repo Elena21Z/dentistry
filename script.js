06bd06d9137cdcdd4c7886f93c987e31d7678d2b
Elena21Z <96683042+Elena21Z@users.noreply.github.com>
2022-08-18 17:27:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let isOpen = false 
 function openMenu() {
     const container = document.getElementById("myContainer")
+    const img = document.getElementById("imgOpen")
     if (isOpen) {
         container.style.display = "none"
         isOpen = false
+        img.src = "/icons8-list-91.png"
+        isOpen = false
     } else {
         container.style.display = "flex"  
+        isOpen = true
+        img.src="/icons8-menu-48.png"
         isOpen = true
     }
 }
